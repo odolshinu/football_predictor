ef797637edaf6bac8de57553c2b44596bd01ad1c
odol shinu <odolshinu@gmail.com>
2014-08-13 01:23:46 +0530

    league admin settings

--- static/js/leagues.js
+++ static/js/leagues.js
@@ -5,3 +5,9 @@ launch_create_league_popup = function() {
 launch_join_league_popup = function() {
 	$("#join_league").modal("show");
 }
+
+group_settings = function(league, code, id) {
+	$("#settings_title").text(league);
+	$("#join_code").val(code);
+	$("#league_settings").modal("show");
+}
