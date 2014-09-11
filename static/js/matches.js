a11e34145675032be3d35ac1939776f13c0b30a7
odol shinu <odolshinu@gmail.com>
2014-09-12 00:49:21 +0530

    automatic according click in 8 seconds

---
+++ static/js/matches.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	var matches = $("#match_accordion h3");
+
+	function switchMatches(arr){
+		console.log(arr);
+	    var i = 1;
+	    setInterval(
+	        function(){
+	            arr[i].click();
+	            i++;
+	            if(i >= matches.length) i = 0;
+	        },8000);
+	}
+
+	switchMatches(matches);
+});
